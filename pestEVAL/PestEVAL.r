d315f2db78f27d14719498e6261f6f943747ce70
Peter Wegmann <wegmannp@hu-berlin.de>
2021-07-08 18:06:40 +0200

    table2 figure version 2

--- pestEVAL/PestEVAL.r
+++ pestEVAL/PestEVAL.r
@@ -2,16 +2,26 @@
 library(ggplot2)
 library(grid)
 
-Path_to_Table2_CSV  ="table2/bench_performance_score_40m_10_short.csv";
+Path_to_Table2_CSV  ="table2/bench_2m_2_comparepest.csv";
 
 #generateTable2 <- function(Path_to_Table2_CSV){
 
 performance_score = read.csv(file = Path_to_Table2_CSV)
 
-number_rows = ceiling(nrow(performance_score)/2)
+performance_score$concat_name = paste(performance_score$benchmark, performance_score$exception)
+
+performance_score$tool_f = factor(performance_score$tool)
+performance_score$concat_name_f = factor(performance_score$concat_name)
+
+max_x = max(performance_score$mtf)
+
+
+
+number_rows = nlevels(performance_score$concat_name_f)
+N_Guidance_V = nlevels(performance_score$tool_f)
 
 performance_score$x = 1;
-performance_score$y = 265;
+performance_score$y = max_x+2;
 
 annotate_rel = character(nrow(performance_score));
 annotate_rel[1] = "Reliability:"
@@ -20,10 +30,10 @@ performance_score$annotate_rel = annotate_rel
 
 performance_score$xreliab = performance_score$x
 
-performance_score$xreliab =  ifelse(performance_score$tool == 'zest',performance_score$xreliab + 1 , performance_score$xreliab)
+performance_score$xreliab =  ifelse(performance_score$tool == 'zest',performance_score$xreliab + (N_Guidance_V-1) , performance_score$xreliab)
+performance_score$xreliab =  ifelse(performance_score$tool == 'pest2',performance_score$xreliab + (N_Guidance_V-2) , performance_score$xreliab)
 
 
-performance_score$concat_name = paste(performance_score$benchmark, performance_score$exception)
 
 
 
@@ -46,34 +56,37 @@ p  <- ggplot(performance_score,aes(fill =tool, y=mtf,x = tool ))+
   ylab("Mean time to Find in s")+
   xlab("")+
   scale_fill_brewer(palette = "Set1") +
-  coord_flip(clip = "off",ylim = c(0,300),xlim = c(1,2))+
+  coord_flip(clip = "off",ylim = c(0,max_x),xlim = c(1,N_Guidance_V))+
   labs(fill ="Guidance Version")+
   theme(strip.text =  element_text(size = rel(1.3),face = "bold", colour = "gray39"),
         axis.text = element_text( size = 14 ),
         axis.text.x = element_text( size = 20 ),
         axis.text.y = element_text( size = 20 ),
-        axis.title = element_text( size = 16, face = "bold" ),
+        axis.title = element_text( size = 20, face = "bold" ),
         legend.title = element_text(size = 19, face = "bold" ),
         legend.text = element_text(size=19),
         plot.margin = unit(c(0.5,5,1,1), "cm")
   )+
-  geom_text(
-  data    = performance_score,
-  mapping = aes(x = x, y = y, label = benchmark),
-  alpha = 1,
-  size = 20,
-  colour = "gray50")+
-  geom_text(
+  geom_text( # Class under Test
     data    = performance_score,
-    mapping = aes(x = xreliab, y = y+85, label = repeatibility ),
+    mapping = aes(x = x, y = y, label = benchmark),
     alpha = 1,
-    size = 10)+
-  geom_text(
-            data = performance_score,
-            colour = "black",
-            mapping = aes(x = xreliab+1.6, y = y+85, label = annotate_rel,fontface = "bold" ),
-            size = 8
-            ) 
+    size = 20,
+    colour = "gray50",
+    hjust = 1
+  )+
+  geom_text( # reliability scores
+    data    = performance_score,
+    mapping = aes(x = xreliab, y = y+10, label = repeatibility ),
+    alpha = 1,
+    size = 10
+  )+
+  geom_text( #reliability label
+    data = performance_score,
+    colour = "black",
+    mapping = aes(x = xreliab+0.6+(N_Guidance_V-1), y = y+10, label = annotate_rel,fontface = "bold" ),
+    size = 8
+  ) 
 
 
 
@@ -83,3 +96,4 @@ ggsave("table2.pdf", plot= p,device = "pdf",path = "imgs/",width = 30,height = 4
 
 
 #generateTable2( Path_to_Table2_CSV  )
+
