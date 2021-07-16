336dab6a484b33ebf8cdca37ad73e2afc8b8d0b5
Peter Wegmann <wegmannp@hu-berlin.de>
2021-07-16 09:52:16 +0200

    r script verbessert

--- pestEVAL/PestEVAL.r
+++ pestEVAL/PestEVAL.r
@@ -10,16 +10,14 @@ library(zoo)
 library(Hmisc)
 library(ggpubr)
 library(lubridate)
+library(cowplot)
 
 
-## specify path to .csv file for Table 2 generation
-Path_to_Table2_CSV  ="table2/bench_8m_25_comparepest.csv";
 
 
-## Directory with benchmark rawdata
-path_bench_data = "results_8m_20_comp"
 
 
+## Generate Table simmilar to Table 2 in ZestPaper
 generateTable2Boxplot <- function(Path_to_Table2_CSV){
   
   performance_score = read.csv(file = Path_to_Table2_CSV)
@@ -29,7 +27,7 @@ generateTable2Boxplot <- function(Path_to_Table2_CSV){
   performance_score$tool_f = factor(performance_score$tool)
   performance_score$concat_name_f = factor(performance_score$concat_name)
   
-  max_x = max(performance_score$mtf)
+  max_x = 400 #max(performance_score$mtf)
   
   
   
@@ -37,7 +35,7 @@ generateTable2Boxplot <- function(Path_to_Table2_CSV){
   N_Guidance_V = nlevels(performance_score$tool_f)
   
   performance_score$x = 1;
-  performance_score$y = max_x+2;
+  performance_score$y =  max_x+10;
   
   xoffset = 60
   
@@ -75,7 +73,8 @@ generateTable2Boxplot <- function(Path_to_Table2_CSV){
     theme(legend.position="top") +
     ylab("Mean time to Find in s")+
     xlab("")+
-    scale_fill_brewer(palette = "Set1") +
+    scale_fill_brewer(palette = "Set2",labels = c("pest/perf.-score","pest/input","zest")) +
+    scale_x_discrete( labels = c("pest" = "perf.-score","zest" = "zest", "pest2" = "input"))+
     coord_flip(clip = "off",ylim = c(0,max_x),xlim = c(1,N_Guidance_V))+
     labs(fill ="Guidance Version")+
     theme(strip.text =  element_text(size = rel(1.3),face = "bold", colour = "gray39"),
@@ -89,9 +88,9 @@ generateTable2Boxplot <- function(Path_to_Table2_CSV){
     )+
     geom_text( # Class under Test
       data    = performance_score,
-      mapping = aes(x = x, y = y, label = benchmark),
+      mapping = aes(x = x+0.2, y = y, label = benchmark),
       alpha = 1,
-      size = 20,
+      size = 17,
       colour = "gray50",
       hjust = 1
     )+
@@ -115,6 +114,8 @@ generateTable2Boxplot <- function(Path_to_Table2_CSV){
   ggsave("table2Box.jpg", plot= p,device = "jpg",path = "imgs/",width = 30,height = 40, units = "cm",dpi = "print")
 }
 
+
+## generates Barplot from the same Data
 generateTable2Barplot <- function(Path_to_Table2_CSV){
   
   performance_score = read.csv(file = Path_to_Table2_CSV)
@@ -172,10 +173,11 @@ generateTable2Barplot <- function(Path_to_Table2_CSV){
     #geom_boxplot()+
     #geom_dotplot(binaxis = 'y', dotsize = 0.3,stackdir = 'center')+
     facet_wrap(~concat_name ,strip.position = "top",nrow = number_rows,labeller = as_labeller(crash_names) )+
+    scale_x_discrete( labels = c("pest" = "perf.-score","zest" = "zest", "pest2" = "input"))+
     theme(legend.position="top") +
     ylab("Mean time to Find in s")+
     xlab("")+
-    scale_fill_brewer(palette = "Set1") +
+    scale_fill_brewer(palette = "Set2",labels = c("pest/perf.-score","pest/input","zest")) +
     coord_flip(clip = "off",ylim = c(0,max_x),xlim = c(1,N_Guidance_V))+
     labs(fill ="Guidance Version")+
     theme(strip.text =  element_text(size = rel(1.3),face = "bold", colour = "gray39"),
@@ -189,9 +191,9 @@ generateTable2Barplot <- function(Path_to_Table2_CSV){
     )+
     geom_text( # Class under Test
       data    = performance_score,
-      mapping = aes(x = x, y = y, label = benchmark),
+      mapping = aes(x = x+0.2, y = y, label = benchmark),
       alpha = 1,
-      size = 20,
+      size = 17,
       colour = "gray50",
       hjust = 1
     )+
@@ -204,7 +206,7 @@ generateTable2Barplot <- function(Path_to_Table2_CSV){
     geom_text( #reliability label
       data = performance_score,
       colour = "black",
-      mapping = aes(x = xreliab+17.9, y = y+xoffset, label = annotate_rel,fontface = "bold" ),
+      mapping = aes(x = xreliab+19.9, y = y+xoffset, label = annotate_rel,fontface = "bold" ),
       size = 8
     ) 
   
@@ -216,12 +218,10 @@ generateTable2Barplot <- function(Path_to_Table2_CSV){
 
 
 
-
-read_Meas_p_z_Data <- function(mydir){
-
-
-
-
+# looks for "plot_data" files in subdirectorys and reads all of them 
+# adds $tool $guidance and $repetition_id collumn 
+# unix_time collumn is also ajusted to start at 0 for each run
+read_Meas_p_z_p2Data <- function(mydir){
 
 
 myfiles_pest_zest <- list.files(path=mydir, pattern="plot_data", full.names=TRUE,recursive = TRUE)
@@ -315,7 +315,7 @@ return(table_z_p)
 }
 
 
-
+# looks for "viz.csv" files in subdirectorys and reads all of them 
 read_Meas_p2_Data <- function(mydir){
 
   myfiles_pest2 <- list.files(path=mydir, pattern="viz.csv", full.names=TRUE,recursive = TRUE)
@@ -398,17 +398,120 @@ read_Meas_p2_Data <- function(mydir){
 
 
 
+#prints plot to /imgs diras .jpg and .pdf  with height and width in cm 
+print_plot <- function(gg_plot,file_str,h,w){
+  ggsave(paste(file_str,".pdf"), plot= gg_plot,device = "pdf",path = "imgs/",width = w,height = h, units = "cm",dpi = "print")
+  ggsave(paste(file_str,".jpg"), plot= gg_plot,device = "jpg",path = "imgs/",width = w,height = h, units = "cm",dpi = "print")
+  
+}
 
 
-generateTable2Boxplot( Path_to_Table2_CSV  )
-generateTable2Barplot( Path_to_Table2_CSV  )
+# plots a var of the table over time  for each fuzzed tool
+plot_data_Variable <- function(data_table,time_interval,colName,ylabel ,scale,leglab){
+  
+  
+  aggregated_tab <- data_table                        %>% 
+    select(guidance,unix_time,tool,.data[[colName]])  %>%
+    group_by(guidance,tool,unix_time,)                %>%
+    mutate(G = floor(unix_time/time_interval))        %>%
+    mutate(unix_time = G * time_interval)             %>%
+    select(guidance,unix_time,tool,.data[[colName]])  %>%
+    summarise(mean = mean(.data[[colName]]),
+              sd = sd(.data[[colName]]),
+              max = mean + sd,
+              min = mean -sd,
+              N = n())
+  var_plot <-  aggregated_tab %>%  ggplot(aes(y = mean ,x = unix_time, ymin = min,ymax = max,color = guidance))+
+    facet_wrap(~tool,nrow = 1,scales = scale)+
+    scale_color_brewer(palette="Set2",labels= leglab,name = "Guidance")+
+    geom_ribbon( alpha = 0.1)+
+    geom_line(size = 1.1)+ 
+    theme_bw()+
+    theme(legend.position="right") +
+    ylab(ylabel)+
+    xlab("time in s")
+  
+  return(var_plot)
+}
+
+
+# plots a var of the table over time  for each fuzzed tool
+plot_data_Variable_omit_pest2 <- function(data_table,data_table2,time_interval,colName,colName2,ylabel ,scale,leglab){
+  
+  
+  
+
+  data_table <- subset(table_z_p, guidance != "pest2")    
+  
+  aggregated_tab <- data_table                        %>% 
+    select(guidance,unix_time,tool,.data[[colName]])  %>%
+    group_by(guidance,tool,unix_time,)                %>%
+    mutate(G = floor(unix_time/time_interval))        %>%
+    mutate(unix_time = G * time_interval)             %>%
+    select(guidance,unix_time,tool,.data[[colName]])  %>%
+    summarise(mean = mean(.data[[colName]]),
+              sd = sd(.data[[colName]]),
+              max = mean + sd,
+              min = mean -sd,
+              N = n())
+  
+  
+  aggregated_tab2 <- data_table2                       %>% 
+    select(guidance,elapsedMilliseconds,tool,.data[[colName2]])  %>%
+    group_by(guidance,tool,elapsedMilliseconds,)                %>%
+    mutate(G = floor(elapsedMilliseconds/time_interval))        %>%
+    mutate(elapsedMilliseconds = G * time_interval)             %>%
+    select(guidance,elapsedMilliseconds,tool,.data[[colName2]])  %>%
+    summarise(mean = mean(.data[[colName2]]),
+              sd = sd(.data[[colName2]]),
+              max = mean + sd,
+              min = mean -sd,
+              N = n())
+  
+  var_plot <-  aggregated_tab %>%  ggplot(aes(y = mean ,x = unix_time, ymin = min,ymax = max,color = guidance))+
+    facet_wrap(~tool,nrow = 1,scales = scale)+
+    scale_color_brewer(palette="Set2",labels= leglab,name = "Guidance")+
+    geom_ribbon( alpha = 0.1)+
+    geom_smooth(data = aggregated_tab2,aes(y = mean ,x = elapsedMilliseconds, ymin = min,ymax = max,color = guidance ))+
+    geom_line(size = 1.1)+ 
+    theme_bw()+
+    theme(legend.position="right") +
+    ylab(ylabel)+
+    xlab("time in s")
+  
+  return(var_plot)
+}
+
+
+
+
+
+##################################################################################################################################
+##################################################################################################################################
+##################################################################################################################################
+
+
+
+## specify path to .csv file for Table 2 generation
+Path_to_Table2_CSV_bar  ="table2/bench_main_1h_10.csv";
+Path_to_Table2_CSV_box  ="table2/bench_main_1h_10_unique_lines.csv";
+
+## Directory with benchmark rawdata
+path_bench_data = "results_main_bench"
+
+
+
+generateTable2Boxplot( Path_to_Table2_CSV_box  )
+generateTable2Barplot( Path_to_Table2_CSV_bar  )
+
+
 
 
 
 
 
 ### Generate Table 2
-table_z_p <- read_Meas_p_z_Data(path_bench_data)
+table_z_p <- read_Meas_p_z_p2Data(path_bench_data)
 table_p2 <- read_Meas_p2_Data(path_bench_data)
 
 
@@ -416,42 +519,11 @@ table_p2 <- read_Meas_p2_Data(path_bench_data)
 
 
 
-print_plot <- function(gg_plot,file_str,h,w){
-  #valid inputs
-  ggsave(paste(file_str,".pdf"), plot= gg_plot,device = "pdf",path = "imgs/",width = w,height = h, units = "cm",dpi = "print")
-  ggsave(paste(file_str,".jpg"), plot= gg_plot,device = "jpg",path = "imgs/",width = w,height = h, units = "cm",dpi = "print")
-  
-}
 
-plot_data_Variable <- function(data_table,time_interval,colName,ylabel ,scale){
-
-  
-  aggregated_tab <- data_table %>% 
-    select(guidance,unix_time,tool,.data[[colName]])%>%
-    group_by(guidance,tool,unix_time,) %>%
-    mutate(G = floor(unix_time/time_interval))%>%
-    mutate(unix_time = G * time_interval) %>%
-    select(guidance,unix_time,tool,.data[[colName]])%>%
-    summarise(mean = mean(.data[[colName]]),
-              sd = sd(.data[[colName]]),
-              max = mean + sd,
-              min = mean -sd,
-              N = n())
-var_plot <-  aggregated_tab %>%  ggplot(aes(y = mean ,x = unix_time, ymin = min,ymax = max,color = guidance))+
-    facet_wrap(~tool,nrow = 1,scales = scale)+
-    scale_color_brewer(palette="Set2")+
-    geom_ribbon( alpha = 0.1)+
-    geom_line(size = 1.1)+ 
-    theme_bw()+
-    theme(legend.position="top") +
-    ylab(ylabel)+
-    xlab("time in s")
-
-return(var_plot)
-}
 
 ## Labels for Legend 
-label_leg = c("pest/performances-core","pest/input","zest")
+label_leg = c("pest/performance-score","pest/input","zest")
+x_tic_labs =  c("pest" = "perf.-score","zest" = "zest", "pest2" = "input")
 
 
 
@@ -460,27 +532,92 @@ w = 60
 
 
 
-paths_total_vec <- c("paths_total","paths totoal","fixed")
+paths_total_vec <- c("paths_total","paths totoal","free")
 
-cycles_done_vec <- c("cycles_done","cycles done","fixed")
+cycles_done_vec <- c("cycles_done","cycles done","free")
 
-execs_per_sec_vec <-c("execs_per_sec","executions per second","fixed")
+execs_per_sec_vec <-c("execs_per_sec","executions per second","free")
 
-unique_crashes_vec <-c("unique_crashes","unique crashes","fixed")
+unique_crashes_vec <-c("unique_crashes","unique crashes","free")
 
+valid_inputs <-c("valid_inputs","valid inputs","free")
 
-
-plot_vec <- list(paths_total_vec,cycles_done_vec,execs_per_sec_vec,unique_crashes_vec)
-
-
-paths_total <- plot_data_Variable(table_z_p,20,paths_total_vec[1],paths_total_vec[2],paths_total_vec[3])  
-
-print_plot(paths_total,paths_total_vec[1],h,w)
-
-plot_data_Variable(table_z_p,20,"valid_inputs","valid Inputs","fixed")  
+invalid_inputs <-c("invalid_inputs","invalid inputs","free")
 
 
-plot_data_Variable(table_z_p,20,"unique_crashes","unique crashes","free")
+var_list <- list(cycles_done_vec,
+                 unique_crashes_vec,
+                 valid_inputs,
+                 invalid_inputs,
+                 execs_per_sec_vec)
+
+
+plot_list <- list()
+i <- 1
+
+for (var in var_list){
+  plotted_var <- plot_data_Variable(table_z_p,100,var[1],var[2],var[3],label_leg)  
+  print_plot(plotted_var,var[1],h,w)
+  
+  plot_list[[i]] <- plotted_var
+  i <- i+1
+  
+}
+
+
+
+
+
+############# Execs per Second VIOLIN PLOT ###########################
+aggregated_exec_per_sec   <- table_z_p                %>%
+
+  select(guidance,unix_time,tool,execs_per_sec)     %>%
+  group_by(guidance,tool)      
+
+plot_list[[length(plot_list)+1]] <- 
+  ggplot(data = aggregated_exec_per_sec,aes(x = guidance, y = execs_per_sec, group = factor(guidance), color = factor(guidance),fill = factor(guidance) ))+
+  facet_wrap(~tool,scales = "free",nrow = 1)+
+  geom_violin(trim = "true")+
+  ylab("executions per second")+
+  scale_x_discrete( labels =x_tic_labs)+
+  theme_bw()+
+  geom_boxplot(width=0.1,fill = "white")+
+  scale_color_brewer(palette="Set2",labels= label_leg,name = "Guidance")+
+  scale_fill_brewer(palette="Set2",labels= label_leg,name = "Guidance")
+
+print_plot(plot_list[[length(plot_list)]],"execs_per_sec_violin",h,w)
+
+
+
+
+
+## saved inputs #####################################
+plot_list[[length(plot_list)+1]] <- plot_data_Variable_omit_pest2(table_z_p,table_p2,20,"paths_total","saved_Inputs","Inputs saved","free",label_leg)
+
+print_plot(plot_list[[length(plot_list)]],"saved_Inputs",h,w)
+
+
+
+
+
+
+major <- plot_grid( plotlist = plot_list,rows = length(plot_list),align = "hv" )
+
+
+
+
+##### Arrange all plots in list in one PDF/JPG ##########################
+
+ggsave("all.pdf", plot= major,device = "pdf",path = "imgs/",width = 50,height = 50, units = "cm",dpi = "print")
+ggsave("all.jpg", plot= major,device = "jpg",path = "imgs/",width = 50,height = 50, units = "cm",dpi = "print")
+
+      
+ 
+
+
+
+
+
 
 
 
@@ -510,11 +647,4 @@ saved_inputs <- ggplot(table_p2,aes( y=saved_Inputs,x = elapsedMilliseconds,colo
 
 
 
-#majorPlot <- ggarrange(valid_inputs, unique_crashes,execs_per_sec,paths_total,cycles_done,
-#                       labels = c("A", "B", "C","D","E"),
-#                       ncol = 1, nrow = 5)
-
-
-#ggsave("all.pdf", plot= majorPlot,device = "pdf",path = "imgs/",width = 50,height = 40, units = "cm",dpi = "print")
-#ggsave("all.jpg", plot= majorPlot,device = "jpg",path = "imgs/",width = 50,height = 40, units = "cm",dpi = "print")
 
